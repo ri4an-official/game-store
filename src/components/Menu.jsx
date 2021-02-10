a54d4fe261163d9b65d76f11f8de6655b4340dc5
Ri4an <redacai.38@gmail.com>
2021-02-10 18:11:58 +0600

    .

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,21 +3,13 @@ import { Nav, Navbar } from "react-bootstrap";
 export const Menu = () => (
     <>
         <Navbar expand="lg">
-            <Navbar.Brand href={`${process.env.PUBLIC_URL}/`}>
-                Game Store
-            </Navbar.Brand>
+            <Navbar.Brand href="/">Game Store</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href={`${process.env.PUBLIC_URL}/basket`}>
-                        Basket
-                    </Nav.Link>
-                    <Nav.Link href={`${process.env.PUBLIC_URL}/login`}>
-                        Sign In
-                    </Nav.Link>
-                    <Nav.Link href={`${process.env.PUBLIC_URL}/register`}>
-                        Sign Up
-                    </Nav.Link>
+                    <Nav.Link href="/basket">Basket</Nav.Link>
+                    <Nav.Link href="/login">Sign In</Nav.Link>
+                    <Nav.Link href="/register">Register</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
--- src/components/games/Games.jsx
+++ src/components/games/Games.jsx
@@ -26,7 +26,7 @@ export const Games = () => {
             <div>
                 <Link
                     className="btn btn-primary noblock right"
-                    to={`${process.env.PUBLIC_URL}/create-game`}
+                    to="/create-game"
                 >
                     Add Game
                 </Link>
