a6b8838f1d80bd358a99ace9321246610e3cc758
Nova-Strife <68599948+Nova-Strife@users.noreply.github.com>
2021-03-09 23:50:10 +0600

    FIX LATER

--- src/components/AddToBasket.tsx
+++ src/components/AddToBasket.tsx
@@ -8,7 +8,7 @@ export const AddToBasket = ({ children }: { children: Game }) => {
     return (
         <span
             onClick={() =>
-                dispatch(addToBasket({ ...children, id: Date.now() }))
+                dispatch(addToBasket({ ...children, id: Date.now() })) //!! отсутствует свойство price
             }
             className="right cart"
         >
