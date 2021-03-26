a380eba7750465d5d456f1b25b9132f97a9c7ee5
Nova-Strife <68599948+Nova-Strife@users.noreply.github.com>
2021-03-26 15:53:50 +0600

    .

--- src/common/redux/games-reducer.ts
+++ src/common/redux/games-reducer.ts
@@ -2,8 +2,7 @@ import { Game } from "./../models/Game";
 import { createSlice } from "@reduxjs/toolkit";
 import { ceil } from "lodash";
 import { Dispatch } from "react";
-import { getGameDetails, getGames, searchGame } from "./api";
-import { buy } from "./login-form";
+import { getGames, searchGame } from "./api";
 const slice = createSlice({
     name: "game-store",
     initialState: {
@@ -39,11 +38,3 @@ export const search = (name: string) => async (dispatch: Dispatch<any>) => {
 
 export default slice.reducer;
 export const { setGames, setFetch } = slice.actions;
-// export const selectGameOnPage = (
-//     setSelectedGame: (game: Game) => void,
-//     slug: string
-// ) => async (dispatch: Dispatch<any>) => {
-//     dispatch(setFetch(true));
-//     setSelectedGame(await getGameDetails(slug));
-//     dispatch(setFetch(false));
-// };
