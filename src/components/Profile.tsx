95a59d276d8684d5545d815da10b8d70d4988c38
Ri4an <redacai.38@gmail.com>
2021-04-07 20:55:49 +0600

    .

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,15 +1,19 @@
-import { useSelector } from "react-redux";
-import { State } from "../common/redux/redux-reducer";
-import { Title } from "./Title";
-import { Games } from "./games/Games";
+import { useSelector } from "react-redux"
+import { State } from "../common/redux/redux-reducer"
+import { Title } from "./Title"
+import { Games } from "./games/Games"
 
 export const Profile = () => {
-    const myGames = useSelector((state: State) => state.login.user.games);
+    const myGames = useSelector((state: State) => state.login.user.games)
     return (
         <>
             <Title>Profile</Title>
             <h4>My Games</h4>
-            <Games>{myGames}</Games>
+            {myGames.length ? (
+                <Games>{myGames}</Games>
+            ) : (
+                <h4 className="red center">You don't have bought games</h4>
+            )}
         </>
-    );
-};
+    )
+}
