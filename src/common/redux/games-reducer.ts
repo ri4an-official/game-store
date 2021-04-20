532df45c5ab4f0ea07af89e1552338adeef6fa7f
Ri4an <redacai.38@gmail.com>
2021-04-20 19:39:46 +0600

    .

--- src/common/redux/games-reducer.ts
+++ src/common/redux/games-reducer.ts
@@ -2,7 +2,7 @@ import { Game } from "./../models/Game"
 import { createSlice } from "@reduxjs/toolkit"
 import { Dispatch } from "react"
 import { getGames } from "./api"
-const slice = createSlice({
+export const slice = createSlice({
     name: "game-store",
     initialState: {
         games: [] as Game[],
@@ -17,13 +17,12 @@ const slice = createSlice({
         },
     },
 })
-
-export const setGamesAsync = (page: number, query?: string) => async (
-    dispatch: Dispatch<any>
-) => {
-    dispatch(setFetch(true))
-    dispatch(setGames(await getGames(page, query)))
-    dispatch(setFetch(false))
+//prettier-ignore
+export const setGamesAsync = (page: number, query?: string) =>
+    async (dispatch: Dispatch<any>) => {
+        dispatch(setFetch(true))
+        dispatch(setGames(await getGames(page, query)))
+        dispatch(setFetch(false))
 }
 export default slice.reducer
 export const { setGames, setFetch } = slice.actions
