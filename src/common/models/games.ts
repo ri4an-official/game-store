c260bd16b60addd5ac93aff3d2725d8635ceb21c
Ri4an <redacai.38@gmail.com>
2021-05-10 20:06:09 +0600

    .

--- src/common/models/games.ts
+++ src/common/models/games.ts
@@ -1,13 +1,15 @@
 import { getGames } from "../redux/api"
-import { combine, createEffect, restore } from "effector"
+import { combine, createEffect, createEvent, forward, restore } from "effector"
 import { Game } from "./Game"
 
 export const fxSetGames = createEffect(
     async ({ page, term }: { page: number; term?: string }) =>
         await getGames(page, term)
 )
+export const setGames = createEvent<{ page: number; term?: string }>()
 export const $games = combine({
     games: restore<Game[]>(fxSetGames, []),
     isFetch: fxSetGames.pending,
     error: restore(fxSetGames.failData, null).reset(fxSetGames.done),
 })
+forward({ from: setGames, to: fxSetGames })
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,17 +6,17 @@ import { Search } from "./Search"
 import useAsyncEffect from "use-async-effect"
 import Pagination from "react-js-pagination"
 import { useHistory, useLocation, useParams } from "react-router"
-import { $games, fxSetGames } from "../common/models/games"
+import { $games, setGames } from "../common/models/games"
 import { useStore } from "effector-react"
 import { Error } from "./../common/error/Error"
 export const Main = () => {
     const { games, isFetch, error } = useStore($games)
     const page = Number(useParams<any>().page ?? 1)
-    const term = useLocation().search.replaceAll("?term=", "")
+    const term = useLocation().search.replaceAll("?search=", "")
     const history = useHistory()
     const [total, setTotal] = useState(0)
     useAsyncEffect(async () => setTotal((await getGamesCount(term)) ?? 1), [term])
-    useAsyncEffect(() => fxSetGames({ page, term }), [page, term])
+    useAsyncEffect(() => setGames({ page, term }), [page, term])
     return isFetch ? (
         <Loader />
     ) : error ? (
@@ -32,7 +32,7 @@ export const Main = () => {
             <p />
             <Pagination
                 onChange={(p) =>
-                    history.push(`/${p === 1 ? "" : p}${term && `?term=${term}`}`)
+                    history.push(`/${p === 1 ? "" : p}${term && `?search=${term}`}`)
                 }
                 totalItemsCount={total}
                 itemsCountPerPage={21}
