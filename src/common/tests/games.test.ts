c0f755a6db845eef7afc089834577915a6fbcab3
Ri4an <vadimguber7@gmail.com>
2021-09-13 12:17:44 +0600

    fix: fill files

--- src/common/tests/games.test.ts
+++ src/common/tests/games.test.ts
@@ -1,26 +1,26 @@
-import { $cart, addGame, deleteGame } from "../models/cart"
-import { Game } from "../models/Game"
-import { fxGetGames } from "../models/games"
-import { $currentUser, buyGames, fxLogin } from "../models/login"
-import { gamesApi } from "../models/api"
+import { $cart, addGame, deleteGame } from '../models/cart'
+import { Game } from '../models/types/Game'
+import { fxGetGames } from '../models/games'
+import { $currentUser, buyGames, fxLogin } from '../models/login'
+import { gamesApi } from '../models/api'
 
 test("Games aren't null", async () => {
     expect((await fxGetGames()).length).toBe(20)
 })
 test("Game details isn't null", async () => {
-    expect((await gamesApi.details("portal")).slug).toBe("portal")
-    expect((await gamesApi.details("bioshock")).slug).toBe("bioshock")
+    expect((await gamesApi.details('portal')).slug).toBe('portal')
+    expect((await gamesApi.details('bioshock')).slug).toBe('bioshock')
 })
 test("Games search isn't null", async () => {
-    expect((await gamesApi.get(1, "portal"))[0].slug).toBe("portal")
+    expect((await gamesApi.get(1, 'portal'))[0].slug).toBe('portal')
 })
-test("Cart works", () => {
+test('Cart works', () => {
     addGame({ id: 1 } as Game)
     expect($cart.getState().length).toBe(1)
     deleteGame(1)
     expect($cart.getState().length).toBe(0)
 })
-test("Profile works", () => {
+test('Profile works', () => {
     buyGames([{ id: 1 }, { id: 2 }] as Game[])
     expect($currentUser.getState().games.length).toBe(2)
 })
--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,20 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const { createServer } = require("http")
-const { port, uri, options } = require("./config.json")
+const express = require('express')
+const mongoose = require('mongoose')
+const { createServer } = require('http')
+const { port, uri, options } = require('./config.json')
 
 const app = express()
 mongoose
     .connect(uri, options)
-    .then(() => console.log("MongoDB connect"))
+    .then(() => console.log('MongoDB connect'))
     .catch(console.log)
-createServer(app).listen(port, () => console.log(`server's up.port: ${port}`))
-module.exports = mongoose.model(
-    "users",
-    mongoose.Schema(app, { name: String, email: String, role: String })
-)
+createServer(app).listen(port, () => console.log('server is up on port:', port))
+
+// const users = mongoose.model(
+//     'users',
+//     mongoose.Schema(app, { name: String, email: String, role: String })
+// )
+const db = mongoose.models
+console.log(db)
+
+module.exports = 'wefweefew'
